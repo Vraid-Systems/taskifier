31fed5523b44d7b0e9553f351bf94956394a7d08
Jason Zerbe <jzerbe@vraidsys.com>
2013-09-08 16:33:34 -0600

    filled out 1) response JSON 2) email delivery for queue worker

--- taskifier/const.py
+++ taskifier/const.py
@@ -2,3 +2,6 @@ RESP_KEY_ID = "id"
 RESP_KEY_SOURCE = "source"
 RESP_KEY_DEST = "dest"
 RESP_KEY_CONTENT = "content"
+
+RESP_STATUS_KEY = "status"
+RESP_STATUS_TEXT_KEY = "status_text"
--- taskifier/internal/pushQueue.py
+++ taskifier/internal/pushQueue.py
@@ -1,6 +1,10 @@
 from datetime import datetime
+from google.appengine.api.app_identity import get_application_id
+from google.appengine.api.mail import send_mail
 from google.appengine.api.taskqueue import Task as queue_Task
+from taskifier import const, getJsonHttpResponse
 from taskifier.models import Task as db_Task
+from taskifier.internal.WorkerHelper import WorkerHelper
 
 def _create_task_from_db(db_Task_obj):
     if db_Task_obj is None:
@@ -18,7 +22,8 @@ def _create_task_from_db(db_Task_obj):
 def cron(request): #called by cron.yaml through django
     db_Task_objs = _get_ready_tasks()
     if db_Task_objs is None:
-        return
+        return getJsonHttpResponse({const.RESP_STATUS_KEY: "OK",
+                                    const.RESP_STATUS_TEXT_KEY: "no stored tasks ready"})
     
     for db_Task_obj in db_Task_objs:
         queue_Task_obj = _create_task_from_db(db_Task_obj)
@@ -26,6 +31,9 @@ def cron(request): #called by cron.yaml through django
             queue_Task_obj.add(queue_name='send', transactional=False)
     
     _delete_processed_tasks(db_Task_objs)
+    
+    return getJsonHttpResponse({const.RESP_STATUS_KEY: "OK",
+                                const.RESP_STATUS_TEXT_KEY: "stored tasks queued"})
 
 def _get_ready_tasks():
     #lte filter --> http://stackoverflow.com/a/4668703
@@ -37,4 +45,17 @@ def _delete_processed_tasks(db_Task_objs):
         db_Task_obj.delete()
 
 def worker(request): #called by queued task
-    return
+    if request.POST and len(request.POST) == 3:
+        workerHelper = WorkerHelper()
+        if workerHelper.isEmail(request.POST.dest):
+            send_mail(sender="app@"+get_application_id()+".appspotmail.com",
+                      to=request.POST.dest,
+                      subject="notification from "+request.POST.source,
+                      body=request.POST.content,
+                      reply_to=request.POST.source)
+            
+            return getJsonHttpResponse({const.RESP_STATUS_KEY: "OK",
+                                        const.RESP_STATUS_TEXT_KEY: "email sent to "+request.POST.dest})
+    
+    return getJsonHttpResponse({const.RESP_STATUS_KEY: "FAIL",
+                                const.RESP_STATUS_TEXT_KEY: "nothing to do"})
